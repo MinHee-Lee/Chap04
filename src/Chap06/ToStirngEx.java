6d2c1aececa6914bd81498e8de22e4d58d351032
MinHee-Lee <57509357+MinHee-Lee@users.noreply.github.com>
2021-10-18 04:04:02 +0900

    (10/22)수업중 실습과제

---
+++ src/Chap06/ToStirngEx.java
@@ -0,0 +1,11 @@
+package Chap06;
+import Chap06.Point;
+
+public class ToStirngEx{
+    public static void main(String[] args) {
+        Point p = new Point(2,3);
+        System.out.println(p.toString());
+        System.out.println(p);
+        System.out.println(p + "입니다.");
+    }
+}
